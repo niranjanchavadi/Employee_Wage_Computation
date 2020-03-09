aa779ad6aae94ec2b55e373aabc5749fd7a3fe62
nnchavadi <nnchavadi2@gmail.com>
2020-03-09 13:24:45 +0530

    [NiranjanChavadi].Refractor write employeecheck present aor absent

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,2 +1,10 @@
+  
 #!/bin/bash -x
-echo "****** WELCOME TO EMPLOYEE WAGES ******"
+isPresent=1
+empCheck=$((RANDOM%2))
+if [ $empCheck -eq $isPresent ]
+then
+	echo Employee is present
+else
+	echo Employee is Absent
+fi
