eb1124c8dc0825fcc285caa3a1dd035e04e4c0a7
nnchavadi <nnchavadi2@gmail.com>
2020-03-09 16:09:39 +0530

    [NiranjanChavadi].Refractor Employee wage calculation using a function

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -6,20 +6,25 @@ WORKING_DAY_PER_MONTH=20
 EMP_WAGE_PER_HR=20
 totalWorkingDays=0
 totalEmployeeHrs=0
+function getWorkHrs() {
+	empCheck=$1
+	case $empCheck in $IS_FULL_TIME )
+      empHrs=8
+      ;;
+      $IS_PART_TIME )
+      empHrs=4
+      ;;
+      * )
+      empHrs=0
+      ;;
+   esac
+	echo $empHrs
+}
 while (( $totalWorkingDays < WORKING_DAY_PER_MONTH && totalEmployeeHrs < $TOTAL_WORKING_HRS ))
 do
 	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
-	case $empCheck in $IS_FULL_TIME )
-		empHrs=8
-		;;
-		$IS_PART_TIME )
-		empHrs=4
-		;;
-		* )
-		empHrs=0
-		;;
-	esac
+	empHrs="$( getWorkHrs $empCheck )"
 	totalEmployeeHrs=$(($totalEmployeeHrs+$empHrs));
 	dailyWage=$(( $empHrs*$EMP_WAGE_PER_HR ))
 	echo $dailyWage
