a5c514f66e39d9336760c6fc6f4187245f0e53b4
nnchavadi <nnchavadi2@gmail.com>
2020-03-09 15:45:04 +0530

    [NiranjanChavadi].Refractor Employee wage calculation wages and month and in hour also

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -x
-isFullTime=1
-isPartTime=2
+IS_FULL_TIME=1
+IS_PART_TIME=2
 EMP_WAGE_PER_HR=20
-empCheck=$((RANDOM%3))
-if [ $empCheck -eq $isFullTime ]
-then
-	empHrs=8
-elif [ $empCheck -eq $isPartTime ]
-then
-	empHrs=4
-else
-	empHrs=0
-fi
-dailyWage=$(( $empHrs*$EMP_WAGE_PER_HR ))
+totalWorkingDays=0
+totalEmployeeHrs=0
+while (( $totalWorkingDays < 20 ))
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3))
+	case $empCheck in $IS_FULL_TIME )
+		empHrs=8
+		;;
+		$IS_PART_TIME )
+		empHrs=4
+		;;
+		* )
+		empHrs=0
+		;;
+	esac
+	totalEmployeeHrs=$(($totalEmployeeHrs+$empHrs));
+done
+dailyWage=$(( $totalEmployeeHrs*$EMP_WAGE_PER_HR ));
 echo $dailyWage
 
